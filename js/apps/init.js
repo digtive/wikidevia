cb91207fe2740ed6348f7ec8c2449307d71bf2ab
bagussjm <bagusibag85@gmail.com>
2019-05-14 01:30:37 +0700

    trending, articel & lab preview showing at home page

---
+++ js/apps/init.js
@@ -0,0 +1,11 @@
+    $(document).ready(function () {
+
+        $('.dropdown-trigger').dropdown({
+            'constrainWidth' : false,
+            'alignment' : 'right'
+        });
+
+        $('.tooltipped').tooltip();
+
+
+    });
